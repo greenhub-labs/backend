aae6ee99a64ebda8859c604cba46e1cedab8842f
JSisques <javierplazasisques@gmail.com>
2025-07-10 10:41:19 +0200

    feat: Add logging to server bootstrap process to display the running URL

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger(AppModule.name);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  const url = await app.getUrl();
+  logger.log(`🚀 Server is running on ${url}`);
 }
 bootstrap();
