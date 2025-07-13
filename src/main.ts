22c7e0ccbac40f2f269c6b8f76132237d4e0e71d
JSisques <javierplazasisques@gmail.com>
2025-07-13 13:01:58 +0200

    fix: Update server port to 4100 in main.ts for application startup

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger(AppModule.name);
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4100);
   const url = await app.getUrl();
   logger.log(`🚀 Server is running on ${url}`);
 }
