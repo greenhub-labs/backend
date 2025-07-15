d54d77175f533c9ef989e8176c07d19a2c2956b9
JSisques <javierplazasisques@gmail.com>
2025-07-15 19:22:46 +0200

    feat: Update GetAllFarms query handler to include farm members with roles in the response

--- src/modules/farms/application/queries/get-all-farms/get-all-farms.query-handler.ts
+++ src/modules/farms/application/queries/get-all-farms/get-all-farms.query-handler.ts
@@ -10,6 +10,10 @@ import {
   FARMS_CACHE_REPOSITORY_TOKEN,
   FarmsCacheRepository,
 } from '../../ports/farms-cache.repository';
+import {
+  FarmMembershipsRepository,
+  FarmMemberWithRole,
+} from '../../ports/farm-memberships.repository';
 
 /**
  * Query handler for GetAllFarmsQuery
@@ -23,6 +27,8 @@ export class GetAllFarmsQueryHandler
     private readonly farmsRepository: FarmsRepository,
     @Inject(FARMS_CACHE_REPOSITORY_TOKEN)
     private readonly farmsCacheRepository: FarmsCacheRepository,
+    @Inject('FARM_MEMBERSHIPS_REPOSITORY_TOKEN')
+    private readonly farmMembershipsRepository: FarmMembershipsRepository,
   ) {}
 
   /**
@@ -30,7 +36,18 @@ export class GetAllFarmsQueryHandler
    * @param query - The query to handle
    * @returns Array of FarmEntity
    */
-  async execute(query: GetAllFarmsQuery): Promise<FarmEntity[]> {
-    return this.farmsRepository.findAll();
+  async execute(
+    query: GetAllFarmsQuery,
+  ): Promise<Array<{ farm: FarmEntity; members: FarmMemberWithRole[] }>> {
+    const farms = await this.farmsRepository.findAll();
+    const results = await Promise.all(
+      farms.map(async (farm) => {
+        const members = await this.farmMembershipsRepository.getUsersByFarmId(
+          farm.id.value,
+        );
+        return { farm, members };
+      }),
+    );
+    return results;
   }
 }
